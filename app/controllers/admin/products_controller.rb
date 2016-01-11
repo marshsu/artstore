af290d8123359933f7cf3d311b22463adabda0c4
Mars Hsu <marshsu@gmail.com>
2016-01-11 18:26:24 +0800

    編輯沒有圖fix

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -26,7 +26,13 @@ class Admin::ProductsController < ApplicationController
 
 	def edit
 		@product = Product.find(params[:id])
-		@photo = @product.photo
+		# @photo = @product.photo
+
+		if @product.photo.present?
+			@photo = @product.photo
+		else
+			@photo = @product.build_photo
+		end
 	end
 
 	def update
